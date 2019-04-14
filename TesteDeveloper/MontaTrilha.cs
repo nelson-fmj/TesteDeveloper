ed1c8a02b1ad41ce3ff480a9ee25e6ab3ec0a728
Nelson Fernandes <nelson.fmj@outlook.com>
2019-04-14 14:38:31 -0300

    Inicial

---
+++ TesteDeveloper/MontaTrilha.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDeveloper
+{
+    class MontaTrilha
+    {
+
+        int max = 0;
+        int soma = 0;
+        int indMax = 0;
+        List<string> listaFormatada;
+        StringBuilder texto = new StringBuilder();
+
+
+        public void Manha(ref List<int> duracao, ref List<string> titulo, ref List<int> duracaoSelcionada, ref List<string> tituloSelcionado)
+        {
+            soma = 0;
+
+            for (int i = 0; i < duracao.Count; i++)
+            {
+                if (soma == 180)
+                    break;
+
+                soma += duracao[i];
+                duracaoSelcionada.Add(duracao[i]);
+                tituloSelcionado.Add(titulo[i]);
+
+                if (soma > 180)
+                {
+                    max = duracaoSelcionada.Max();
+                    indMax = duracaoSelcionada.IndexOf(max);
+
+                    soma -= max;
+                    duracaoSelcionada.RemoveAt(indMax);
+                    tituloSelcionado.RemoveAt(indMax);
+                }
+            }
+
+            // REMOVE O QUE FOI USADO
+            for (int i = 0; i < duracaoSelcionada.Count; i++)
+            {
+                duracao.Remove(duracaoSelcionada[i]);
+                titulo.Remove(tituloSelcionado[i]);
+            }
+        }
+
+        public void Tarde(ref List<int> duracao, ref List<string> titulo, ref List<int> duracaoSelcionada, ref List<string> tituloSelcionado)
+        {
+            soma = 0;
+
+            for (int i = 0; i < duracao.Count; i++)
+            {
+                if (soma == 240)
+                    break;
+
+                soma += duracao[i];
+                duracaoSelcionada.Add(duracao[i]);
+                tituloSelcionado.Add(titulo[i]);
+
+                if (soma > 240)
+                {
+                    max = duracaoSelcionada.Max();
+                    indMax = duracaoSelcionada.IndexOf(max);
+
+                    soma -= max;
+                    duracaoSelcionada.RemoveAt(indMax);
+                    tituloSelcionado.RemoveAt(indMax);
+                }
+            }
+
+            // REMOVE O QUE FOI USADO
+            for (int i = 0; i < duracaoSelcionada.Count; i++)
+            {
+                duracao.Remove(duracaoSelcionada[i]);
+                titulo.Remove(tituloSelcionado[i]);
+            }
+        }
+
+        public List<string> FormataSaidaManha(List<int> duracaoSelecionada, List<string> tituloSelecionado)
+        {
+            listaFormatada = new List<string>();
+            TimeSpan horaInicial = new TimeSpan(9, 0, 0);
+
+            texto.Append($"{horaInicial.ToString(@"hh\:mm")} ").Append(tituloSelecionado[0]).Append($" {duracaoSelecionada[0]}mim");
+            listaFormatada.Add(texto.ToString());
+            horaInicial = horaInicial.Add(new TimeSpan(0, duracaoSelecionada[0], 0));
+            texto.Clear();
+            for (int i = 1; i < duracaoSelecionada.Count; i++)
+            {
+                texto.Append($"{horaInicial.ToString(@"hh\:mm")} ").Append(tituloSelecionado[i]).Append($" {duracaoSelecionada[i]}mim");
+                horaInicial = horaInicial.Add(new TimeSpan(0, duracaoSelecionada[i], 0));
+                listaFormatada.Add(texto.ToString());
+                texto.Clear();
+            }
+            texto.Append($"{horaInicial.ToString(@"hh\:mm")} ").Append("Almoço");
+            listaFormatada.Add(texto.ToString());
+            texto.Clear();
+
+            return listaFormatada;
+        }
+
+        public List<string> FormataSaidaTarde(List<int> duracaoSelecionada, List<string> tituloSelecionado)
+        {
+            listaFormatada = new List<string>();
+            TimeSpan horaInicial = new TimeSpan(13, 0, 0);
+
+            texto.Append($"{horaInicial.ToString(@"hh\:mm")} ").Append(tituloSelecionado[0]).Append($" {duracaoSelecionada[0]}mim");
+            listaFormatada.Add(texto.ToString());
+            horaInicial = horaInicial.Add(new TimeSpan(0, duracaoSelecionada[0], 0));
+            texto.Clear();
+            for (int i = 1; i < duracaoSelecionada.Count; i++)
+            {
+                texto.Append($"{horaInicial.ToString(@"hh\:mm")} ").Append(tituloSelecionado[i]).Append($" {duracaoSelecionada[i]}mim");
+                horaInicial = horaInicial.Add(new TimeSpan(0, duracaoSelecionada[i], 0));
+                listaFormatada.Add(texto.ToString());
+                texto.Clear();
+            }
+            texto.Append($"{horaInicial.ToString(@"hh\:mm")} ").Append("Evento de Networking");
+            listaFormatada.Add(texto.ToString());
+            texto.Clear();
+
+            return listaFormatada;
+        }
+    }
+}
---
+++ TesteDeveloper/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDeveloper
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<String> titulo = new List<String>();
+            List<int> duracao = new List<int>();
+
+            List<int> duracaoSelecionada = new List<int>();
+            List<String> tituloSelecionado = new List<String>();
+
+            List<string> trilha01 = new List<string>();
+            List<string> trilha02 = new List<string>();
+
+            List<string> listaAux = new List<string>();
+
+            MontaTrilha montar = new MontaTrilha();
+
+            string linha = string.Empty;
+
+            
+            // CARREEGAR LISTA COM O ARQUIVO DE ENTRADA 
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Entrada.txt"))
+                {
+                    while((linha = sr.ReadLine()) != null)
+                    {
+                        string[] aux = linha.Split(';');
+                        titulo.Add(aux[0].Trim());
+                        duracao.Add(Convert.ToInt32(aux[1].Trim()));
+                    }
+                   
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao ler o arquivo:");
+                Console.WriteLine(e.Message);
+            }
+
+
+            // TRILHA 01
+            montar.Manha(ref duracao, ref titulo, ref duracaoSelecionada, ref tituloSelecionado);
+            trilha01 = montar.FormataSaidaManha(duracaoSelecionada, tituloSelecionado);
+
+            duracaoSelecionada.Clear();
+            tituloSelecionado.Clear();
+
+            montar.Tarde(ref duracao, ref titulo, ref duracaoSelecionada, ref tituloSelecionado);
+            listaAux = montar.FormataSaidaTarde(duracaoSelecionada, tituloSelecionado);
+            trilha01.AddRange(listaAux);
+
+
+            duracaoSelecionada.Clear();
+            tituloSelecionado.Clear();
+
+            // TRILHA 02
+            montar.Manha(ref duracao, ref titulo, ref duracaoSelecionada, ref tituloSelecionado);
+            trilha02 = montar.FormataSaidaManha(duracaoSelecionada, tituloSelecionado);
+
+            duracaoSelecionada.Clear();
+            tituloSelecionado.Clear();
+
+            montar.Tarde(ref duracao, ref titulo, ref duracaoSelecionada, ref tituloSelecionado);
+            listaAux = montar.FormataSaidaTarde(duracaoSelecionada, tituloSelecionado);
+            trilha02.AddRange(listaAux);
+
+
+
+            Console.WriteLine("Trilha 01");
+            Console.WriteLine("=========");
+            foreach (string n in trilha01)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Trilha 02");
+            Console.WriteLine("=========");
+            foreach (string n in trilha02)
+            {
+                Console.WriteLine(n);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
